6a3761a021dac60a34b9158fad052f52330447f6
daniel-f82 <169076225+daniel-f82@users.noreply.github.com>
2024-05-27 16:52:18 -0600

    Update views.py

--- bdd01/educa/courses/views.py
+++ bdd01/educa/courses/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from braces.views import CsrfExemptMixin ,  JsonRequestResponseMixin
+from django.apps import apps
 
-# Create your views here.
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    PermissionRequiredMixin,
+)
+
+from django.forms.models import modelform_factory
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views.generic.base import TemplateResponseMixin, View
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic.list import ListView
+
+from .forms import ModuleFormSet
+from .models import Content, Course, Module
+
+class OwnerMixin:
+     def get_queryset(self):
+         qs = super().get_queryset()
+         return qs.filter(owner=self.request.user)
+    
+
+class OwnerEditMixin:
+    def form_valid(self, form):
+          form.instance.owner = self.request.user
+          return super().form_valid(form)
+      
+class OwnerCourseMixin(
+     OwnerMixin , LoginRequiredMixin, PermissionRequiredMixin
+):
+     model = Course
+     fiels = ['subject', 'title', 'slug', 'overview']
+     success_url = reverse_lazy('manage_course_list')
+
